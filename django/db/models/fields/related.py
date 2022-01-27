25808977c0989cf24c12dcf17c23f8e7ea60a99a
Jermel Babb <developers@octane5.net>
2022-01-27 01:25:50 -0500

    set on_delete default for OneToOneField

--- django/db/models/fields/related.py
+++ django/db/models/fields/related.py
@@ -1044,7 +1044,7 @@ class OneToOneField(ForeignKey):
 
     description = _("One-to-one relationship")
 
-    def __init__(self, to, on_delete, to_field=None, **kwargs):
+    def __init__(self, to, on_delete=CASCADE, to_field=None, **kwargs):
         kwargs['unique'] = True
         super().__init__(to, on_delete, to_field=to_field, **kwargs)
 
